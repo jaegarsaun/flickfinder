5cbd4838a5717363887f4d4e5a29c051ca911b86
Jaegar Saunders <jaegarsaunders@192.168.219.9>
2024-01-29 09:55:43 -0330

    created hero

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'dark': '#0e0e28',
+        'primary': '#FA58B6',
+        
+      },
     },
   },
   plugins: [],
